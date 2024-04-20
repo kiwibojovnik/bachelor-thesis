c6aa84bb8947e5239cadfc0dd7fcd9e0412cfaad
daliborkyjovsky <xkyjov03@vutbr.cz>
2024-04-20 11:10:05 +0200

    dodělana implementace ipv6

--- tests/call_test.py
+++ tests/call_test.py
@@ -52,18 +52,17 @@ class WebConnectivityTester:
 
             if dns_result[0] == "OK":
                 ip_address = tests_IPv6.get_ip_address(dns_result[1], self.ip_type)
-                print(ip_address)
                 tcp_result = tests_IPv6.tcp_handshake(ip_address)
                 ping_result = tests_IPv6.ping_test(ip_address)
-                trace_hop = tests_IPv6.perform_trace6(ip_address)
-             #   redirect = tests_IPv6.detect_redirect(address)
-             #   http_status, content_length, headers, html_content = tests_IPv6.http_get_request(address)
-             #   certificate = tests_IPv6.get_https_certificate(ip_address, address)
+                trace_hop = tests_IPv6.perform_trace(ip_address)
+                redirect = tests_IPv6.detect_redirect(address, self.ip_type)
+                http_status, content_length, headers, html_content = tests_IPv6.http_get_request(address)
+                certificate = tests_IPv6.get_https_certificate(ip_address, address)
 
                 end_time = time.time()
                 duration = round(end_time - start_time, 2)
-            #    output_content = reformat_url.extract_domain(address) + '_content.html'
-             #   self.save_html_content(output_content, html_content)
+                output_content = reformat_url.extract_domain(address) + '_content.html'
+                self.save_html_content(output_content, html_content)
 
                 return CaseInsensitiveDict({
                     'Time': duration,
@@ -77,14 +76,15 @@ class WebConnectivityTester:
                     'PING Status': ping_result[0],
                     'PING IP': ping_result[1],
                     'Trace hop IP': trace_hop,
-             #       'Redirected Status': redirect[0],
-             #       'Redirected Location': redirect[1],
-             #       'HTTP Status': http_status,
-             #       'Content Length': content_length,
-             #       'Headers': headers,
-             #       'HTML Content': output_content,
-             #       'Cert Status': certificate[0],
-             #       'Cert Content': certificate[1],
+                    'Redirected Status': redirect[0],
+                    'Redirected Location': redirect[1],
+                    'Redirected Location IPs': redirect[2],
+                    'HTTP Status': http_status,
+                    'Content Length': content_length,
+                    'Headers': headers,
+                    'HTML Content': output_content,
+                    'Cert Status': certificate[0],
+                    'Cert Content': certificate[1],
                     'Timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 })
 
--- tests/tests_IPv6.py
+++ tests/tests_IPv6.py
@@ -4,22 +4,14 @@
 # Popis: Tento skript upravuje csv soubor tak, aby vždi jako první argument byla uvedena url adresa.
 # Verze Pythonu: 3.9
 
-
-import ssl
-
 # Importing necessary libraries
-import requests
-import time
-
+import requests, ssl
 from scapy.all import *
 from timeout_decorator import timeout
-
 from utils import reformat_url
 
 function_timeout = 300
 
-
-# TODO: opravit errory ve vystupu - asi odstranit nefunguje to s něma
 
 @timeout(function_timeout)
 def resolver_identification():
@@ -38,12 +30,12 @@ def resolver_identification():
         return "N/A", "N/A"
 
     except socket.gaierror as e:
-        print(f"RESOLVER ERROR: {e}")
-        return "RESOLVER ERROR: " + str(e), "N/A"
+        print("RESOLVER ERROR: " + str(e))
+        return "RESOLVER ERROR", "N/A"
 
 
 @timeout(function_timeout)
-def dns_lookup(address, ip):
+def dns_lookup(address, ip_type):
     """
     Performs DNS lookup for a given website.
 
@@ -52,20 +44,19 @@ def dns_lookup(address, ip):
 
     Args:
         address (str): Website to perform DNS lookup for. Either in the format www.example.com or example.com.
-        ip (str):
+        ip_type (str): Type of the ip (IPv4 or IPv6)
 
     Returns:
         tuple: A tuple containing the status ('OK' or 'N/A') and a list of IP addresses (both IPv4 and IPv6).
     """
     try:
         domain = reformat_url.extract_domain(address)
-        print(domain)
 
         # Retrieve IPv4 addresses
         ip_addresses_ipv4 = socket.gethostbyname_ex(domain)[2]
 
         # Attempt to retrieve IPv6 addresses
-        if ip == "ipv6":
+        if ip_type == "ipv6":
             ip_addresses_ipv6 = socket.getaddrinfo(domain, None, socket.AF_INET6)
             ipv6_addresses = [addr[4][0] for addr in ip_addresses_ipv6]
 
@@ -82,7 +73,7 @@ def dns_lookup(address, ip):
 
     except Exception as e:
         # Print an error message if there is an issue with DNS lookup
-        print("DNS lookup error:", e)
+        print("DNS lookup error:" + str(e))
         return "N/A", "N/A"
 
     except TimeoutError:
@@ -169,31 +160,42 @@ def calculate_checksum(data):
 
 
 @timeout(function_timeout)
-def get_https_certificate(ip_address, url, port=443):
+def get_https_certificate(ip_address, address, port=443):
     """
     Retrieves the HTTPS certificate for a given IP address and domain.
 
     Args:
         ip_address (str): The IP address to check (IPv4).
-        domain (str): The domain associated with the IP address.
+        address (str): The domain associated with the IP address.
         port (int, optional): The port number to connect to. Defaults to 443 for HTTPS.
 
     Returns:
         tuple: A tuple containing the status ('OK', 'Failed', or 'N/A') and the certificate or error message.
     """
     try:
+        # Extract IPv4 address from a mapped IPv6 address if necessary
+        ip_address = extract_ipv4_from_mapped_ipv6(ip_address)
+
+        # Determine the IP address type
+        ip_type = check_ip_address_type(ip_address)
+
+        if ip_type == "ipv4":
+            sock_type = socket.AF_INET
+        else:
+            sock_type = socket.AF_INET6
+
         # Create a default SSL context
         context = ssl.create_default_context()
 
         # Create a socket connection to the specified IP address
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        with socket.socket(sock_type, socket.SOCK_STREAM) as sock:
             # If the port is 443 (default for HTTPS), attempt to retrieve the certificate
             if port == 443:
                 sock.settimeout(10)
 
                 sock.connect((ip_address, port))
 
-                with context.wrap_socket(sock, server_hostname=reformat_url.extract_domain(url)) as ssock:
+                with context.wrap_socket(sock, server_hostname=reformat_url.extract_domain(address)) as ssock:
                     # Obtain the certificate from the socket
                     cert = ssock.getpeercert()
                     return "OK", cert
@@ -204,8 +206,8 @@ def get_https_certificate(ip_address, url, port=443):
 
     except Exception as e:
         # Print an error message if there is an issue retrieving the certificate
-        print(f"Error retrieving certificate for address {ip_address}: {e}")
-        return "N/A", str(e)
+        print("Error retrieving certificate" + str(e))
+        return "N/A", "N/A"
 
     except TimeoutError:
         print("Retrieving certificate exceeded timeout.")
@@ -253,11 +255,11 @@ def tcp_handshake(destination_ip, destination_port=80):
 
     except Exception as e:
         # Print an error message if there is an issue during the handshake
-        print(f"TCP handshake error for address {destination_ip}: {e}")
+        print("TCP handshake error: " + str(e))
         return "Failed", "N/A"
 
     except TimeoutError:
-        print("TCP connection test to {}:{} exceeded timeout of 5 seconds")
+        print("TCP connection test exceeded timeout.")
         return "N/A", "N/A"
 
 
@@ -275,14 +277,19 @@ def http_get_request(url):
         If an error occurs, it returns the error message followed by three 'N/A' strings.
     """
     try:
-        # Attempt an HTTP GET request with IPv6 support
-        response = requests.get(reformat_url.add_http(url),
-                                headers={'Host': reformat_url.add_http(url)})
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
+        }
+
+        session = requests.Session()
+        response = session.get(reformat_url.add_http(url),
+                               headers=headers)
+
         return (response.status_code, len(response.content),
                 response.headers, response.text)
 
     except requests.exceptions.RequestException as e:
-        print(f"HTTP GET request to {url} failed: {e}")
+        print("HTTP GET request failed: " + str(e))
         return str(e), "N/A", "N/A", "N/A"
 
     except TimeoutError:
@@ -291,12 +298,13 @@ def http_get_request(url):
 
 
 @timeout(function_timeout)
-def detect_redirect(url):
+def detect_redirect(url, ip_type):
     """
     Detects whether an HTTP request to the specified URL results in a redirect.
 
     Args:
         url (str): The URL to test for redirection.
+        ip_type (str):
 
     Returns:
         tuple: A tuple containing the status ('Redirected' or 'Not redirected') and the redirection target URL.
@@ -308,17 +316,18 @@ def detect_redirect(url):
 
         # Check if the request was redirected
         if response.is_redirect:
-            return "Redirected", response.headers['Location']
+            ip_redirection = dns_lookup(response.headers['Location'], ip_type)
+            return "Redirected", response.headers['Location'], ip_redirection
         else:
-            return "Not redirected", "N/A"
+            return "Not redirected", "N/A", "N/A"
 
     except Exception as e:
-        print("Redirect test error:", e)
-        return "N/A", "N/A"
+        print("Redirect test error: " + str(e))
+        return "N/A", "N/A", "N/A"
 
     except TimeoutError:
         print("Redirect test exceeded timeout.")
-        return "N/A", "N/A"
+        return "N/A", "N/A", "N/A"
 
 
 def extract_ipv4_from_mapped_ipv6(ipv6_address):
@@ -434,37 +443,60 @@ def ping_test(ip_address):
         return "N/A", "N/A"
 
     except Exception as e:
-        print(f"PING TEST ERROR: {e}")
+        print("PING TEST ERROR: " + str(e))
         return "N/A", "N/A"
 
 
-@timeout(function_timeout)
 def perform_trace(ip_address):
     """
-    Performs a traceroute to the specified address.
+    Performs a traceroute to the specified IPv6 address.
 
     Args:
-        ip_address (str): The IP address to trace the route to.
+        ip_address (str): The IPv6 or IPv4 address to trace the route to.
 
     Returns:
         list: A list of tuples containing the IP address of each hop and the round-trip time.
     """
     max_hops = 30
     port = 33434
-    # ICMP Echo Request packet with a type of 8 (echo) and code of 0
-    icmp_echo_request = b'\x08\x00' + struct.pack('!HH', os.getpid() & 0xFFFF, 1)
     trace_result = []
-    # Convert hostname to IP address
-    destination_address = socket.gethostbyname(ip_address)
+
+    # Extract IPv4 address from a mapped IPv6 address if necessary
+    ip_address = extract_ipv4_from_mapped_ipv6(ip_address)
+
+    # Determine the IP address type
+    ip_type = check_ip_address_type(ip_address)
+
+    # Define settings based on IP type
+    settings = {
+        "ipv4": {
+            "socket_type": socket.AF_INET,
+            "socket_iproto": socket.IPPROTO_IP,
+            "socket_uni": socket.IP_TTL,
+            "icmp_echo_request": b'\x08\x00' + struct.pack('!HH', os.getpid() & 0xFFFF, 1),
+            "sock_sock": socket.IPPROTO_ICMP
+        },
+        "ipv6": {
+            "socket_type": socket.AF_INET6,
+            "socket_iproto": socket.IPPROTO_IPV6,
+            "socket_uni": socket.IPV6_UNICAST_HOPS,
+            "icmp_echo_request": b'',
+            "sock_sock": socket.getprotobyname('ipv6-icmp')
+        }
+    }
+
+    # Get the correct settings for the current IP type
+    current_settings = settings[ip_type]
 
     for ttl in range(1, max_hops + 1):
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as send_socket:
+        with socket.socket(current_settings["socket_type"], socket.SOCK_DGRAM, socket.IPPROTO_UDP) as send_socket:
             # Set the time-to-live for the packet
-            send_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+            send_socket.setsockopt(current_settings["socket_iproto"], current_settings["socket_uni"], ttl)
             # Send an empty packet to the address and port
-            send_socket.sendto(icmp_echo_request, (ip_address, port))
+            send_socket.sendto(current_settings["icmp_echo_request"], (ip_address, port))
 
-        with socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) as recv_socket:
+        with socket.socket(current_settings["socket_type"], socket.SOCK_RAW,
+                           current_settings["sock_sock"]) as recv_socket:
             # Set a timeout for the receive operation
             recv_socket.settimeout(1)
             try:
@@ -476,8 +508,7 @@ def perform_trace(ip_address):
                 trace_result.append((addr[0], end_time - start_time))
 
                 # Check if we have reached the destination server
-                if addr[0] == destination_address:
-                    print(f"Reached destination: {addr[0]}")
+                if addr[0] == ip_address:
                     break  # Exit the loop upon reaching the destination
 
             except socket.timeout:
@@ -485,61 +516,3 @@ def perform_trace(ip_address):
                 pass
 
     return trace_result
-
-
-def perform_trace6(ip_address):
-    """
-    Performs a traceroute to the specified IPv6 address.
-
-    Args:
-        ip_address (str): The IPv6 address to trace the route to.
-
-    Returns:
-        list: A list of tuples containing the IP address of each hop and the round-trip time.
-    """
-    max_hops = 30
-    port = 33434
-    trace_result = []
-
-    try:
-        # Extract IPv4 address from a mapped IPv6 address if necessary
-        ip_address = extract_ipv4_from_mapped_ipv6(ip_address)
-
-        # Determine the IP address type
-        ip_type = check_ip_address_type(ip_address)
-
-        destination_address = socket.getaddrinfo(ip_address, None, socket.AF_INET6)[0][4][0]
-
-        for ttl in range(1, max_hops + 1):
-            with socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as send_socket:
-                # Set the time-to-live for the packet
-                send_socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_UNICAST_HOPS, ttl)
-                # Send an empty packet to the address and port
-                send_socket.sendto(b'', (destination_address, port))
-
-            with socket.socket(socket.AF_INET6, socket.SOCK_RAW, socket.getprotobyname('ipv6-icmp')) as recv_socket:
-                # Set a timeout for the receive operation
-                recv_socket.settimeout(1)
-                try:
-                    start_time = time.time()
-                    # Receive the packet
-                    packet, addr = recv_socket.recvfrom(512)
-                    end_time = time.time()
-                    # Append the address and round-trip time to the result
-                    trace_result.append((addr[0], end_time - start_time))
-
-                    # Check if we have reached the destination server
-                    if addr[0] == destination_address:
-                        print(f"Reached destination: {addr[0]}")
-                        break  # Exit the loop upon reaching the destination
-
-                except socket.timeout:
-                    # If no response is received, continue to the next hop
-                    pass
-
-        return trace_result
-
-    except Exception as e:
-        print(f"TRACEROUTE TEST ERROR: {e}")
-        return "N/A", "N/A"
-
